a38cb43855b6c6b6dd8844d5b901ecbb145c438e
Jason Baumann <jason@theseb.de>
2023-12-17 20:11:27 +0100

    First game code

---
+++ src/main.py
@@ -0,0 +1,93 @@
+import pygame
+from tiles import TileMap
+from spritesheet import Spritesheet
+from player import Player
+
+class Game:
+    """
+    bringing together all the important elements of the game
+    includes the main loop through which everything runs
+    """
+    def __init__(self):
+        pygame.init()
+        self.DISPLAY_W, self.DISPLAY_H = 1480, 270
+        self.canvas = pygame.Surface((self.DISPLAY_W, self.DISPLAY_H))
+        self.window = pygame.display.set_mode(((self.DISPLAY_W, self.DISPLAY_H)))
+        self.running = True
+        self.clock = pygame.time.Clock()
+        self.TARGET_FPS = 60
+
+        self.spritesheet = Spritesheet('sprites/placeholder/spritesheet.png')
+        self.player = Player()
+     
+        #tilemap        
+        self.map = TileMap('sprites/placeholder/big_level.csv', self.spritesheet, (0,0))
+        self.player.position.x, self.player.position.y = self.map.start_x, self.map.start_y
+        
+        #camera
+        self.offset = pygame.math.Vector2()
+        self.half_w = self.canvas.get_size()[0]//2
+        self.half_h = self.canvas.get_size()[1]//2
+
+    def handle_events(self):
+        """
+        method to handle all event of the game (keys)
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.player.LEFT_KEY = True
+                elif event.key == pygame.K_RIGHT:
+                    self.player.RIGHT_KEY = True
+                elif event.key == pygame.K_SPACE:
+                    self.player.jump()
+
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    self.player.LEFT_KEY = False
+                elif event.key == pygame.K_RIGHT:
+                    self.player.RIGHT_KEY = False
+                elif event.key == pygame.K_SPACE:
+                    if self.player.is_jumping:
+                        self.player.velocity.y *= .25
+                        self.player.is_jumping = False
+    
+    def center_target_camera(self,target):
+        self.offset.x = (target.rect.centerx - self.half_h)
+        self.offset.y = (target.rect.centery - self.half_w)
+    
+    def update(self, dt):
+        self.center_target_camera(self.player)
+        self.map = TileMap('sprites/placeholder/big_level.csv', self.spritesheet, (self.offset.x + 50, 0))
+        self.player.update(dt, self.map.tiles)
+        
+
+    def draw(self):
+        """
+        rendering everything that is shown in the game 
+        """
+        self.canvas.fill((0, 180, 240))
+        self.map.draw_map(self.canvas)
+        self.player.draw(self.canvas)
+        self.window.blit(self.canvas, (0, 0))
+        pygame.display.update()
+
+    def run(self):
+        """
+        game loop
+        """
+        while self.running: #main loop
+            dt = self.clock.tick(60) * .001 * self.TARGET_FPS         
+            self.handle_events()
+            self.update(dt)
+            self.draw()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
+    pygame.quit()
+    
+#TODO Do it so that the 'camera' correctly follows the player and not just halfway as it is now.
---
+++ src/tiles.py
@@ -0,0 +1,82 @@
+import pygame, csv, os
+
+class Tile(pygame.sprite.Sprite):
+    """
+    single tiles
+    """
+    def __init__(self, image, x, y, spritesheet):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = spritesheet.parse_sprite(image)
+        # Manual load in: self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+class TileMap():
+    def __init__(self, filename, spritesheet, offset:tuple):
+        self.offset = offset
+        self.tile_size = 16
+        self.start_x, self.start_y = 0, 0
+        self.spritesheet = spritesheet
+        self.tiles = self.load_tiles(filename)
+        self.map_surface = pygame.Surface((self.map_w, self.map_h))
+        self.map_surface.set_colorkey((0, 0, 0))
+        self.load_map()
+
+    def draw_map(self, surface):
+        surface.blit(self.map_surface,(0,0))
+
+    def load_map(self):
+        """
+        rendering the tilemap        
+        """
+        for tile in self.tiles:
+            tile.draw(self.map_surface)
+
+    def read_csv(self, filename):
+        """
+        splitting the individual contents of the CSV file
+
+        Args:
+            filename (str): file of the CSV file
+
+        Returns:
+            list: split conten
+        """
+        map = []
+        with open(os.path.join(filename)) as data:
+            data = csv.reader(data, delimiter=',')
+            for row in data:
+                map.append(list(row))
+        return map
+
+    def load_tiles(self, filename):
+        """
+        reads the CSV file for the precise placement of individual tiles in the tilemap
+        Args:
+            filename (str): filename of the CSV file
+
+        Returns:
+            list: list of all tiles
+        """
+        tiles = []
+        map = self.read_csv(filename)
+        x, y = 0, 0
+        for row in map:
+            x = 0
+            for tile in row:
+                if tile == '0':
+                    self.start_x, self.start_y = x * self.tile_size - self.offset[0], y * self.tile_size -  self.offset[1]
+                elif tile == '1':
+                    tiles.append(Tile('grass.png', x * self.tile_size - self.offset[0], y * self.tile_size - self.offset[1], self.spritesheet))
+                elif tile == '2':
+                    tiles.append(Tile('grass2.png', x * self.tile_size - self.offset[0], y * self.tile_size - self.offset[1], self.spritesheet))
+                    # Move to next tile in current row
+                x += 1
+
+            # Move to next row
+            y += 1
+            # Store the size of the tile map
+        self.map_w, self.map_h = x * self.tile_size, y * self.tile_size
