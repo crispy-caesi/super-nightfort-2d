32983696e8b869b2f7bc5a35a69b035ffa5a2d54
crispy-caesus <114518720+crispy-caesus@users.noreply.github>
2024-04-18 23:08:01 +0200

    Audio restructuring

---
+++ src/Audio.py
@@ -0,0 +1,54 @@
+import pygame
+
+class MenuMusicController():
+    """
+    Class handling the music in the menus, allows to start and stop the music, constructor takes background music file path as an argument. 
+    """
+    def __init__(self, musicFilePath :str):
+        self.__backgroundMusic = pygame.mixer.Sound(musicFilePath)
+
+    def play(self):
+        """
+        Play set background music indefinitely
+        """
+        self.__backgroundMusic.play(loops=-1)    # loops=-1 -> loops indefinitely
+    def stop(self):
+        """
+        Stops playing background music
+        """
+        self.__backgroundMusic.stop()
+
+class GameMusicController():
+    """
+    Class handling the music in the game, allows to start and stop the music, constructor takes background music file path as an argument. 
+    """
+
+    def __init__(self, musicFilePath :str):
+        self.__backgroundMusic = pygame.mixer.Sound(musicFilePath)
+
+    def play(self):
+        """
+        Play set background music indefinitely
+        """
+        self.__backgroundMusic.play(loops=-1)    # loops=-1 -> loops indefinitely
+
+    def stop(self):
+        """
+        Stops playing background music
+        """
+        self.__backgroundMusic.stop()
+
+class GameSounds():
+    """
+    Class handling all sound effects played ingame, constructor takes sound effect file path as an argument.
+    """
+    def __init__(self, jumpSoundFilePath :str):
+        self.__jumpSound = pygame.mixer.Sound(jumpSoundFilePath)
+
+    def playJumpSound(self):
+        """
+        Plays jump sound once
+        """
+        self.__jumpSound.play()
+
+#TODO set sounds and music to the same volume
